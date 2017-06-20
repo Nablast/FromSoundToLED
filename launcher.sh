0a4f5761a213070f8a6c787bf54f1cd82a1d1158
Nablast <favre.marinet@gmail.com>
2017-06-20 20:08:26 +0000

    test

---
+++ launcher.sh
@@ -0,0 +1,17 @@
+cd /
+
+cd usr/sbin/
+
+/usr/sbin/alsactl --file /usr/share/doc/audioInjector/asound.state.RCA.thru.test restore
+
+cd /
+
+cd home/pi/Desktop/LED/FromSoundToLed/
+
+git merge master
+
+git
+
+git commit -m 
+
+sudo python LedRaspberry.py &
