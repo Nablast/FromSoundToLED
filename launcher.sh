6653b1c27d94024beba417422df658068fe05014
Nablast <favre.marinet@gmail.com>
2017-06-20 22:28:10 +0200

    Remove bad lines

--- launcher.sh
+++ launcher.sh
@@ -8,12 +8,14 @@ cd /
 
 cd home/pi/Desktop/LED/FromSoundToLed/
 
+git branch logBranch
+
 git merge master
 
 git add --all
 
 git commit -m 
 
-git push https://Nablast:GBtWSKgJ3@github.com/Nablast/FromSoundToLed.git 
+git push -u https://User:Password@github.com/Nablast/FromSoundToLed.git logBranch 
 
 sudo python LedRaspberry.py &
