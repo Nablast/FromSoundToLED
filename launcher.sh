a019ed785ad8b9bc0932fd1518e1017a915db3d9
Nablast <favre.marinet@gmail.com>
2017-12-02 15:18:43 +0000

    remove from launcher

--- launcher.sh
+++ launcher.sh
@@ -4,9 +4,9 @@ cd home/pi/Desktop/LED/FromSoundToLed/
 
 # git pull 
 
-#!/bin/bash
-#until sudo python LedRaspberry.py; do
-#    echo "'LedRaspberry.py' crashed with exit code $?. Restarting..." >&2
-#    sleep 1
-#done
+!/bin/bash
+until sudo python LedRaspberry.py; do
+    echo "'LedRaspberry.py' crashed with exit code $?. Restarting..." >&2
+    sleep 1
+done
 
